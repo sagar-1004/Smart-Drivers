2b4073c3e79e0d96461bf49a5536a0a8c3fc5ab9
Sagar <sagarrahangdale1004@gmail.com>
2023-01-03 14:37:05 +0530

    driver model created

--- SmartDrivers/drivers/backend/models.py
+++ SmartDrivers/drivers/backend/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import uuid
 
+# # Create your models here.
+
+# # Model for details of plots
+
+
+class driver(models.Model):
+    id = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=50)
+    address = models.DateField(auto_now=True)
+    phone = models.CharField(max_length=10)

1db6a79d1364059ac4bdb192314b2f9d32f94559
Kiran <kiranspatil3663@gmail.com>
2023-01-04 13:56:11 +0530

    Created models and superuser

--- SmartDrivers/drivers/backend/models.py
+++ SmartDrivers/drivers/backend/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 import uuid
 
 # # Create your models here.
@@ -14,3 +13,20 @@ class driver(models.Model):
     lname = models.CharField(max_length=50)
     address = models.DateField(auto_now=True)
     phone = models.CharField(max_length=10)
+
+class Hospital(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    longitude = models.DecimalField(decimal_places=8,max_digits=12)
+    lattitude = models.DecimalField(decimal_places=8,max_digits=12)
+    address = models.CharField(max_length=400)
+    bed = models.IntegerField()
+    ventilator = models.IntegerField()
+    dialysis = models.IntegerField()
+
+class Notification(models.Model):
+    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    hospitalID = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    gender = models.CharField(max_length=5)
+    bloodgroup = models.CharField(max_length=5)
+    

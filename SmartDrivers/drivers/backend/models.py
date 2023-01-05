73d769bcbc29f756c0227ec72d28af1c064210db
Kiran <kiranspatil3663@gmail.com>
2023-01-05 15:51:17 +0530

    changes in notification and hospital model

--- SmartDrivers/drivers/backend/models.py
+++ SmartDrivers/drivers/backend/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 import uuid
 
-# # Create your models here.
-
-# # Model for details of plots
-
 
 class driver(models.Model):
     id = models.UUIDField(
@@ -20,13 +16,18 @@ class Hospital(models.Model):
     longitude = models.DecimalField(decimal_places=8,max_digits=12)
     lattitude = models.DecimalField(decimal_places=8,max_digits=12)
     address = models.CharField(max_length=400)
-    bed = models.IntegerField()
-    ventilator = models.IntegerField()
-    dialysis = models.IntegerField()
+    ICU_bed = models.PositiveIntegerField(default=0)
+    bed = models.PositiveIntegerField(default=0)
+    ventilator = models.PositiveIntegerField(default=0)
+    dialysis = models.PositiveIntegerField(default=0)
+    Anesthesia_machine = models.PositiveIntegerField(default=0)
+    
 
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     hospitalID = models.ForeignKey(Hospital,on_delete=models.CASCADE)
     gender = models.CharField(max_length=5)
     bloodgroup = models.CharField(max_length=5)
+    diabetic = models.BooleanField(default=False)
+    blood_pressure_patient = models.BooleanField(default=False)
     
